a72c361d0480ab39365103cf728d3886c3e50c77
xuweiguo <weiguoxu@outlook.com>
2021-09-02 11:11:04 +0800

    Initial commit

---
+++ internal/usecase/interfaces.go
@@ -0,0 +1,27 @@
+// Package usecase implements application business logic. Each logic group in own file.
+package usecase
+
+import (
+	"context"
+
+	"github.com/evrone/go-clean-template/internal/entity"
+)
+
+type (
+	// Translation -.
+	Translation interface {
+		Translate(context.Context, entity.Translation) (entity.Translation, error)
+		History(context.Context) ([]entity.Translation, error)
+	}
+
+	// TranslationRepo -.
+	TranslationRepo interface {
+		Store(context.Context, entity.Translation) error
+		GetHistory(context.Context) ([]entity.Translation, error)
+	}
+
+	// TranslationWebAPI -.
+	TranslationWebAPI interface {
+		Translate(entity.Translation) (entity.Translation, error)
+	}
+)
